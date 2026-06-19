0d9a100e04df4f362d50a793bfb8b06ae328698f
keysi.quirós <keysi.quirs@AUTOP-F6YYX32YV>
2026-06-19 11:56:42 -0600

    Agregar archivos de proyecto.

---
+++ UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace SS_003_Babel_Swag_Labs
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
